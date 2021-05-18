ce0ce3acf26ce9d1c104b153e4d799e2587f64ac
Mohak Sharma <mohak327@gmail.com>
2021-05-18 13:32:38 +0530

    exp7 - line fitting

---
+++ exp7/LineFitting.cpp
@@ -0,0 +1,42 @@
+// Write a program to fit the equation of a line in te from of (y = ax + b) fro the following data.
+
+// x : 1     2     3     4     5
+// y : 14	 27    40    55    68
+
+#include<iostream>
+#include<math.h>
+
+#define N 50
+using namespace std;
+
+int main() {
+    int n;
+    float x[N], y[N], sumX=0, sumX2=0, sumY=0, sumXY=0, a, b;
+
+    cout<<"\nNo. of data points:  ";
+    cin >> n;   //  ATQ, n=5
+
+    cout << "Enter " << n <<" values of x: ";
+    for(int i=1; i<=n; i++) {
+        cin >> x[i];
+    }
+
+    cout << "Enter " << n <<" values of y: ";
+    for(int i=1; i<=n; i++) {
+        cin >> y[i];
+    }
+
+    for(int i=1;i<=n;i++) {
+        sumX = sumX +x[i];
+        sumX2 = sumX2 +x[i]*x[i];
+        sumY = sumY +y[i];
+        sumXY = sumXY +x[i]*y[i];
+    }
+
+    a=((sumX2*sumY -sumX*sumXY)*1.0/(n*sumX2-sumX*sumX)*1.0);
+    b=((n*sumXY-sumX*sumY)*1.0/(n*sumX2-sumX*sumX)*1.0);
+
+    cout << "Values are: a = " << a << " and b = " << b;
+
+ return(0);
+}
